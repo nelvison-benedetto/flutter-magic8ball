8e107cfce588b554c519df4abac40062eecd1eb6
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-03 09:58:29 +0100

    project done

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color.fromRGBO(69, 151, 249, 1),
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(42, 80, 164, 1),
-          title: Center(
-            child: Text(
-              'Ask me something',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-        body: MainPage(),
-      ),
-    ),
+  runApp(
+      MainPage()
   );
 }
 
@@ -31,10 +13,46 @@ class MainPage extends StatefulWidget{
 }
 
 class _MainPageState extends State<MainPage>{
+
+  int num = 1;
+  void incrementNum(){
+     num = Random().nextInt(5)+1;
+    //if (num < 5){ num++;}
+    //else{ num = 1 ;}
+  }
+
   @override
   Widget build(BuildContext context){
-    return Center(
-      child: Text('Hello Flutt!!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(69, 151, 249, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(42, 80, 164, 1),
+          toolbarHeight: 70,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontFamily : "Poppins-Regular",
+            fontSize: 32, // Ora dovresti vedere la modifica della dimensione
+            fontWeight: FontWeight.w900,
+          ),
+          title: Center(
+            child: Text('Ask me something'),
+          ),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed:(){
+                setState(() {
+                  incrementNum(); print(num);
+                });
+                },
+              child: Image.asset('assets/img/ball${num}.png'),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
